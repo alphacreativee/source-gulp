673c71fcb464554ac0012694d9a893a1cf0ceb7b
Gary <hai.alphacreative@gmail.com>
2025-07-06 08:40:34 +0700

    add lenis

--- assets/js/index/script.js
+++ assets/js/index/script.js
@@ -1,5 +1,13 @@
 import { preloadImages } from "../../libs/utils.js";
+// setup lenis
+const lenis = new Lenis();
+lenis.on("scroll", ScrollTrigger.update);
+gsap.ticker.add((time) => {
+  lenis.raf(time * 1000);
+});
 
+gsap.ticker.lagSmoothing(0);
+// end lenis
 const init = () => {
   gsap.registerPlugin(ScrollTrigger);
 };
